a7bd06a8a7ccd70195f5086542c261bc556d797c
Federico Toro Restrepo <federicotoro23141@gmail.com>
2025-09-29 13:00:20 -0500

    config route, and expresion regular for compare the route to correct use

--- Routes/web.php
+++ Routes/web.php
@@ -6,11 +6,19 @@ require_once '../autolader.php';
 use Lib\Route;
 
 
-
 Route::get('/',function(){
-    echo "hola desde la pagina principal";
+    return [
+        'title' => 'Home',
+        'content' => 'Hola desde la pagina de inicio'
+    ];
 });
 
 Route::get('/contact',function(){
     echo "hola desde la pagina contacto";
 });
+
+Route::get('/course/:slug', function($slug){
+     echo "hola desde la pagina cursos, el curso es ".$slug;
+});
+
+Route::dispatch();
--- lib/Route.php
+++ lib/Route.php
@@ -6,12 +6,48 @@ class Route {
 
     //agregar rutas get a routes
     public static function get($uri, $callback){
+        $uri = trim($uri,'/');
         self::$routes['GET'][$uri] = $callback;      
     }
 
     //agregar rutas post a routes como es static no usamos $this-> sino self::
     public static function post($uri, $callback){
+        $uri = trim($uri,'/');
     self::$routes['POST'][$uri] = $callback;
+    }
+
+    public static function dispatch(){
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri = trim($uri,'/');
+        //echo $uri;
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        
+        foreach (self::$routes[$method] as $route => $callback){
+
+
+            if(strpos($route, ':') !== false)
+            {
+                $route = preg_replace('#:[a-zA-Z]+#','([a-zA-Z]+)',$route);          
+            }
+
+            if(preg_match("#^$route$#",$uri,$matches)){
+
+                $params = array_slice($matches,1);
+                //echo json_encode($params);
+                $response = $callback(...$params );
+
+                if(is_array($response) || is_object($response)){
+                    echo json_encode($response);
+                }else{
+                    echo $response;
+                }
+
+                return;
+            }
+          
+        }
+        echo '404 not found';
     }
 
 }
